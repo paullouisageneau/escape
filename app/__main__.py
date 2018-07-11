4798fe3e85c1ce27e50a16efd68cfec0dddd1a8a
Paul-Louis Ageneau <paul-louis@ageneau.org>
2018-07-11 20:31:39 +0200

    Replaced wsgi by pywsgi

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,7 @@
 
 import sys
-import gevent.wsgi
+
+from gevent.pywsgi import WSGIServer
 
 from . import app
 
@@ -10,7 +11,7 @@ def main():
 	try:
 		# Run the webapp on specified port
 		print("Listening on http://127.0.0.1:{}/".format(port))
-		http_server = gevent.wsgi.WSGIServer(("127.0.0.1", port), app)
+		http_server = WSGIServer(("127.0.0.1", port), app)
 		http_server.serve_forever()
 	except KeyboardInterrupt:
 		return 0
