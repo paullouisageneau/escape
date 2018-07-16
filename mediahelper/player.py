afdbf44a28d50853db39b0651cf493f85cd9785f
Paul-Louis Ageneau <paul-louis@ageneau.org>
2018-07-16 23:09:35 +0200

    Fixed player path resolution

--- mediahelper/player.py
+++ mediahelper/player.py
@@ -1,13 +1,12 @@
 
-import os
 import subprocess
 
 class Player:
 	def __init__(self, media_path):
-		self._media_path = media_path
+		self._media_path = media_path.rstrip('/')
 	
 	def play(self, path):
-		location = path if '://' in path else os.path.join(self._media_path, path)
+		location = path if '://' in path else self._media_path + '/' + path.lstrip('/')
 		
 		print('Playing media at location: {}'.format(location))
 		self._run(['omxplayer', '-b', '-o', 'hdmi', location])
