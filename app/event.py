a6b60fbdfd8831a4c9dda6de6f0ad1d34a4fdeec
Paul-Louis Ageneau <paul-louis@ageneau.org>
2018-07-28 13:28:36 +0200

    Added keepalive to SSE events

--- app/event.py
+++ app/event.py
@@ -1,27 +1,34 @@
 
-from gevent import spawn
+from gevent import spawn, sleep
 from gevent.queue import Queue
 
 RETRY = 1000
+KEEPALIVE_INTERVAL = 10000
 
 class EventStream:
-    def __init__(self):
-        self._subscriptions = []
+	def __init__(self):
+		self._subscriptions = []
+		def keepalive():
+			while True:
+				sleep(KEEPALIVE_INTERVAL/1000.)
+				self.publish('keepalive', '')
+		spawn(keepalive)
 
-    def publish(self, event, data):
-        def notify():
-            for s in self._subscriptions:
-                s.put((event, data))
-        spawn(notify)
-        return len(self._subscriptions)
+	def publish(self, event, data):
+		if self._subscriptions:
+			def notify():
+				for s in self._subscriptions:
+					s.put((event, data))
+			spawn(notify)
+		return len(self._subscriptions)
 
-    def subscribe(self):
-        q = Queue()
-        self._subscriptions.append(q)
-        try:
-            yield "retry: {}\n\n".format(RETRY)
-            while True:
-                event, data = q.get()
-                yield "event: {}\ndata: {}\n\n".format(event, data)
-        except GeneratorExit:
-            self._subscriptions.remove(q)
+	def subscribe(self):
+		q = Queue()
+		self._subscriptions.append(q)
+		try:
+			yield "retry: {}\n\n".format(RETRY)
+			while True:
+				event, data = q.get()
+				yield "event: {}\ndata: {}\n\n".format(event, data)
+		except GeneratorExit:
+			self._subscriptions.remove(q)
