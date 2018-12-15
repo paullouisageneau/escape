390a1ae74e8de8162bf6e54ee9e423d731ae4413
Paul-Louis Ageneau <paul-louis@ageneau.org>
2018-12-15 13:20:53 +0100

    Listen on any address

--- app/__main__.py
+++ app/__main__.py
@@ -10,8 +10,8 @@ port = 8080
 def main():
 	try:
 		# Run the webapp on specified port
-		print("Listening on http://127.0.0.1:{}/".format(port))
-		http_server = WSGIServer(("127.0.0.1", port), app)
+		print("Listening on port {}".format(port))
+		http_server = WSGIServer(("0.0.0.0", port), app)
 		http_server.serve_forever()
 	except KeyboardInterrupt:
 		return 0
